d8c29a3d1149f87394f551ad8982866f604ed6b7
KelvinHay <119087884+KelvinHay@users.noreply.github.com>
2023-12-07 15:21:37 -0500

    upload

---
+++ demo.py
@@ -0,0 +1,9 @@
+
+
+# Lets make a robot Barista
+# She will have a name
+# She will greet the customers
+# She will offer a menu 
+# She will collect the order
+# Collect payment 
+# Alert the customer when the order is complete 
